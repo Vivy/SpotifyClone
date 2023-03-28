3cd4dd2c0cc97605894eee76cea2f122efd93301
Vivy <Tomitaviorel@yahoo.com>
2023-03-28 13:10:29 +0300

    feat: layout complete

--- src/component/footer/footer.style.js
+++ src/component/footer/footer.style.js
@@ -1,3 +1,6 @@
 import styled from 'styled-components';
 
-export const Footer = styled.div``
+export const Footer = styled.div`
+  background-color: #181818;
+  height: 100%;
+`
--- src/component/spotify/spotify.style.js
+++ src/component/spotify/spotify.style.js
@@ -5,15 +5,21 @@ export const Spotify = styled.div`
   max-height: 100vh;
   overflow: hidden;
   display: grid;
-  grid-template-columns: 85vh 15vh;
+  grid-template-rows: 85vh 15vh;
 `
 export const SpotifyBody = styled.div`
   display: grid;
   grid-template-columns: 15vw 85vw;
-`
+  height: 100%;
+  width: 100%;
+  background: linear-gradient(transparent, rgba(0,0,0,1));
+  background-color: rgb(32,87,100);
+  `
 
 export const Body = styled.div`
-
+  height: 100%;
+  width: 100%;
+  overflow: auto;
 `
 
 export const BodyContent = styled.div``
